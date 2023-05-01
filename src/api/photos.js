9dee0c1fb7d9400f0ded8e37fe09f5fa323c9780
Watcher1986 <deniss2007@gmail.com>
2023-05-01 12:10:34 +0300

    retrieve bundle for the gh pages

--- src/api/photos.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export async function getPhotos(page) {
-  const { data } = await axios.get(
-    `https://picsum.photos/v2/list?page=${page}&limit=10`
-  );
-  return data;
-}
--- src/utils/parseAndAppend.js
+++
@@ -1,8 +0,0 @@
-export const parseAndAppend = async (template, node) => {
-  const parser = new DOMParser();
-  const docFragment = parser.parseFromString(template, 'text/html').body;
-
-  while (docFragment.firstChild) {
-    await node.appendChild(docFragment.firstChild);
-  }
-};
